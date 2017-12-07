263dcee3e988b8d4266e526ea93772db568ce09d
vocrsz <vocrsz@gmail.com>
2017-12-07 17:49:14 +0300

    Add env_config option

--- lib/indocker/application_initializer.rb
+++ lib/indocker/application_initializer.rb
@@ -7,6 +7,7 @@ class Indocker::ApplicationInitializer
   inject :docker_api
   inject :registry_authenticator
   inject :config
+  inject :envs_loader
 
 
   def init_app(current_path, env: :development)
@@ -14,6 +15,8 @@ class Indocker::ApplicationInitializer
 
     require(config_locator.locate(current_path))
 
+    ENV.update( envs_loader.parse(File.join(current_path, config.env_file)).to_hash )
+    debugger
     # registry_authenticator.authenticate!()
   end
 end
